2365b42e052791bf789f967f9eb89fe7e908d322
LAPTOP-QEJT3KSG\HP <rahmadwimaulia.16@gmail.com>
2025-01-13 18:03:44 +0700

    [update]

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,9 +9,9 @@ class ContactController extends Controller
 {
     function contact(Request $req){
         $contacts = new Contact;
-        $contacts->email = trim($req->input('email'), '"');
+        $contacts->email = trim($req->input('email'), '');
         $contacts ->message=$req->input('message');
-        // $contacts ->pictures=$req->input('pictures');
+        $contacts ->pictures=$req->input('pictures');
         $contacts ->save();
         return $contacts;
     }
